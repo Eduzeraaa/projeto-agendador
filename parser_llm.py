f4b209111da0a096701d2153091546c61f6d174f
Eduardo <eduardoluisbsb2@gmail.com>
2026-03-30 09:13:09 -0300

    Update parser_llm.py

--- parser_llm.py
+++ parser_llm.py
@@ -2,12 +2,17 @@ import os
 from langchain_groq import ChatGroq
 from pydantic import BaseModel, Field
 from dotenv import load_dotenv
+import datetime
 
 load_dotenv()
 
+data = datetime.date.today()
+ano = data.year
+
 class Saida_esperada(BaseModel):
-    data: str = Field(description='Data agendada, modelo americano: yyyy-mm-dd. Se o usuário não informar a data, assuma que data = "..."')
+    data: str = Field(description=f'Data agendada, modelo americano: yyyy-mm-dd. Se o usuário não informar a data, assuma que data = "...". Se o usuário não informar o ano, assuma que o ano é {ano}')
     horario: str = Field(description='Horário agendado, desta forma: HH:MM:SS. Se o usuário não informar o horário, assuma que horario = "..."')
+    telefone: str = Field(description='Telefone do cliente, desta forma: XX 9XXXXXXXX. Se ele te passar o número, é a mesma coisa que telefone. Se o usuário não informar o telefone, assuma que telefone = "..."')
 
 
 # cria o modelo só pro output parser
